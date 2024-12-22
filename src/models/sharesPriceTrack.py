4b30fbc309076870fb51477d5ff2b095c89ce73f
wfgmiami <alpenchev@yahoo.com>
2024-12-22 18:20:37 -0500

    more files

--- src/models/sharesPriceTrack.py
+++ src/models/sharesPriceTrack.py
@@ -10,6 +10,19 @@ import holidays
 from dateutil.relativedelta import relativedelta
 import json
 
+current_year = date.today().year
+year_5yr_ago = current_year - 5
+us_holidays = holidays.UnitedStates(years=[year_5yr_ago, current_year])
+
+
+# Function to find the closest previous business day
+def adjust_to_business_day(target_date, holiday_list):
+    while (
+        target_date.weekday() in (5, 6) or target_date in holiday_list
+    ):  # 5 = Saturday, 6 = Sunday
+        target_date -= timedelta(days=1)
+    return target_date
+
 
 with open("../data/tickers.json", "r") as file:
     tickers_object = json.load(file)
@@ -31,7 +44,7 @@ numQters = 16  # last 4 years
 sh_decreased_tickers = {}
 sharesByQtrs = pd.DataFrame()
 dfs = []
-ticker = "NVDA"
+# ticker = "NVDA"
 
 for ticker in tickers:
     # print(f"{ticker}")
@@ -54,36 +67,12 @@ for ticker in tickers:
                 (companyConcept.json()["units"]["shares"])
             )
 
-            # shares10K = sharesData[
-            #     (sharesData["form"] == "10-K") & (sharesData["frame"].notna())
-            # ]
-
-            # shares10KForm = sharesData[sharesData["form"] == "10-K"]
-
-            # shares10K = (
-            #     shares10KForm.groupby(["start", "end", "val"])
-            #     .first()
-            #     .reset_index()[["start", "end", "val", "fy"]]
-            # )
-
-            # shares10K["fy"] = shares10K["fy"].apply(lambda x: str(x)[2:] + "Q4")
-
-            # shares10K.rename(columns={"fy": "frame"}, inplace=True)
-            # shares10K = shares10K.loc[:, ["frame", "val"]]
-
-            # shares10Q = sharesData[
-            #     (sharesData["form"] == "10-Q") & (sharesData["frame"].notna())
-            # ]
-            # shares10Q = shares10Q.loc[:, ["frame", "val"]]
-
             sharesFilings = sharesData[
                 (
                     ((sharesData["form"] == "10-Q") & (sharesData["frame"].notna()))
                     | (sharesData["form"] == "10-K")
                 )
             ]
-            # print(sharesFilings.tail(16))
-            # print(sharesFilings[sharesFilings["form"] == "10-K"].tail(20))
 
             sharesFilings = (
                 sharesFilings.groupby(["start", "end", "val"], sort=False)
@@ -114,8 +103,6 @@ for ticker in tickers:
                 axis=1,
             )
 
-            # print(sharesFilings.tail(16))
-
             commShOut = sharesFilings.loc[:, ["fy", "val"]]
 
             commShOutQtrs = commShOut.tail(numQters)
@@ -136,8 +123,6 @@ for df in dfs:
     df_standardized = pd.concat([df_standardized, df["fy"]], axis=1)
     df_standardized.set_index("fy", inplace=True)
     dfs_standardized.append(df_standardized)
-
-# print(df_standardized)
 
 
 def calculate_trend_line(dataframes):
@@ -161,7 +146,7 @@ def calculate_trend_line(dataframes):
             slopes[column] = model.coef_[0][0]
             df[f"{column} Trend"] = model.predict(time).flatten()
 
-        slopes_all[f"{column}"] = {"slope": slopes[column]}
+        slopes_all[f"{column}"] = {"slope": slopes[column].round(4)}
         trend_results.append(df)
 
     return slopes_all, trend_results
@@ -170,13 +155,13 @@ def calculate_trend_line(dataframes):
 shares_slopes, df_with_trends = calculate_trend_line(dfs_standardized)
 steepest_decline = min(shares_slopes.items(), key=lambda x: x[1]["slope"])
 
-print("Stocks with the steepest share reduction:")
-print(f"  Product: {steepest_decline[0]}")
-print(f"  Slope: {steepest_decline[1]['slope']:.4f}")
+# print("Stocks with the steepest share reduction:")
+# print(f"  Product: {steepest_decline[0]}")
+# print(f"  Slope: {steepest_decline[1]['slope']:.4f}")
 
-print("\nSlopes for all stocks:")
-for ticker, slope in shares_slopes.items():
-    print(f"{ticker}: Slope = {slope:.4f}")
+# print("\nSlopes for all stocks:")
+# for ticker, slope in shares_slopes.items():
+#     print(f"{ticker}: Slope = {slope:.4f}")
 
 shares_slopes_sorted = dict(sorted(shares_slopes.items(), key=lambda x: x[1]["slope"]))
 shares_reduction_slope = pd.DataFrame.from_dict(
@@ -186,108 +171,28 @@ shares_reduction_slope.rename(
     columns={"slope": f"{numQters/4:.0f} yrs slope"}, inplace=True
 )
 
-# # Plot each product's price trend and trend line
-# plt.figure(figsize=(12, 8))
-
-# for product, prices in product_prices.items():
-#     time = np.arange(len(prices))  # Time steps (X-axis)
-
-#     # Calculate slope and trend line
-#     slope, trend_line = calculate_trend_line(prices)
-
-#     # Plot original data
-#     plt.plot(time, prices, marker="o", label=f"{product} (Slope: {slope:.2f})")
-
-#     # Plot trend line
-#     plt.plot(time, trend_line, linestyle="--", label=f"{product} Trend Line")
-
-# # Customize the plot
-# plt.title("Product Price Trends with Regression Lines", fontsize=16)
-# plt.xlabel("Time (e.g., Days or Months)", fontsize=12)
-# plt.ylabel("Price", fontsize=12)
-# plt.legend(fontsize=10, loc="best")  # Add legend
-# plt.grid(True)
-# plt.show()
-
-
 # 2. Get historical prices and find performance against index
-
-
-# Function to find the closest previous business day
-def adjust_to_business_day(target_date, holiday_list):
-    while (
-        target_date.weekday() in (5, 6) or target_date in holiday_list
-    ):  # 5 = Saturday, 6 = Sunday
-        target_date -= timedelta(days=1)
-    return target_date
-
-
-indexETF = "SPY"
-ticker = "ICE"
-
-current_date = date.today()
-one_year_ago = current_date - timedelta(days=365)
-
-us_holidays = holidays.UnitedStates(years=[one_year_ago.year])
-adjusted_one_year_ago = adjust_to_business_day(one_year_ago, us_holidays)
-
-stockPrice = yf.download(
-    ticker,
-    start=f"{adjusted_one_year_ago}",
-    end=f"{adjusted_one_year_ago + relativedelta(days=1)}",
-)["Adj Close"][f"{ticker}"]
-stockPriceBeg = stockPrice.iloc[0]
-
-stockPrice = yf.download(
-    ticker, start=f"{current_date}", end=f"{current_date + relativedelta(days=1)}"
-)["Adj Close"][f"{ticker}"]
-stockPriceEnd = stockPrice.iloc[0]
-
-stockReturnOneYear = (((stockPriceEnd - stockPriceBeg) / stockPriceBeg) * 100).round(2)
-
-indexPrice = yf.download(
-    indexETF,
-    start=f"{adjusted_one_year_ago}",
-    end=f"{adjusted_one_year_ago+ relativedelta(days=1)}",
-)["Adj Close"][f"{indexETF}"]
-indexPriceBeg = indexPrice.iloc[0]
-
-indexPrice = yf.download(
-    indexETF,
-    start=f"{current_date}",
-    end=f"{current_date+ relativedelta(days=1)}",
-)["Adj Close"][f"{indexETF}"]
-indexPriceEnd = indexPrice.iloc[0]
-
-indexReturnOneYear = (((indexPriceEnd - indexPriceBeg) / indexPriceBeg) * 100).round(2)
-
-print(
-    f"{ticker} one year return vs SPY = {(stockReturnOneYear - indexReturnOneYear).round(2)}"
-)
-
-
-with open("../data/tickers.json", "r") as file:
-    tickers_object = json.load(file)
-
-tickers = tickers_object["Best2016"]
-
 indexETF = "SPY"
 # ticker = "CHRT"
-current_date = date.today()
+beg_period_date = adjust_to_business_day(date(date.today().year, 1, 2), us_holidays)
+endDate = adjust_to_business_day(date.today(), us_holidays)
 # In case of a specific date: Year, Month, Day
-current_date = date(2016, 12, 30)  # must be business date
-one_year_ago = current_date - timedelta(days=365)
-us_holidays = holidays.UnitedStates(years=[one_year_ago.year])
+# current_date = date(2016, 12, 30)  # must be business date
+one_year_ago = endDate - timedelta(days=365)
+two_year_ago = endDate - timedelta(days=730)
 adjusted_one_year_ago = adjust_to_business_day(one_year_ago, us_holidays)
+adjusted_two_year_ago = adjust_to_business_day(two_year_ago, us_holidays)
+return_periods = ["YTD", "OneYear", "TwoYears"]
+
 perf_rank = {}
 
-for ticker in tickers:
-    # print(f"{ticker}")
+
+def getPeriodReturn(begDate, endDate, period, ticker):
     try:
         stockPrice = yf.download(
             ticker,
-            start=f"{adjusted_one_year_ago}",
-            end=f"{adjusted_one_year_ago + relativedelta(days=1)}",
+            start=f"{begDate}",
+            end=f"{begDate + relativedelta(days=1)}",
             progress=False,
         )["Adj Close"][f"{ticker}"]
 
@@ -296,16 +201,16 @@ for ticker in tickers:
         else:
             stockPrice = yf.download(
                 ticker,
-                start=f"{adjusted_one_year_ago}",
-                end=f"{adjusted_one_year_ago + relativedelta(days=1)}",
+                start=f"{begDate}",
+                end=f"{begDate + relativedelta(days=1)}",
                 progress=False,
             )["Adj Close"][f"{ticker}"]
             stockPriceBeg = stockPrice.iloc[0]
 
             stockPrice = yf.download(
                 ticker,
-                start=f"{current_date}",
-                end=f"{current_date + relativedelta(days=1)}",
+                start=f"{endDate}",
+                end=f"{endDate + relativedelta(days=1)}",
                 progress=False,
             )["Adj Close"][f"{ticker}"]
             stockPriceEnd = stockPrice.iloc[0]
@@ -316,16 +221,16 @@ for ticker in tickers:
 
             indexPrice = yf.download(
                 indexETF,
-                start=f"{adjusted_one_year_ago}",
-                end=f"{adjusted_one_year_ago + relativedelta(days=1)}",
+                start=f"{begDate}",
+                end=f"{begDate + relativedelta(days=1)}",
                 progress=False,
             )["Adj Close"][f"{indexETF}"]
             indexPriceBeg = indexPrice.iloc[0]
 
             indexPrice = yf.download(
                 indexETF,
-                start=f"{current_date}",
-                end=f"{current_date + relativedelta(days=1)}",
+                start=f"{endDate}",
+                end=f"{endDate + relativedelta(days=1)}",
                 progress=False,
             )["Adj Close"][f"{indexETF}"]
             indexPriceEnd = indexPrice.iloc[0]
@@ -335,13 +240,30 @@ for ticker in tickers:
             ).round(2)
 
             perf_rank[f"{ticker}"] = {
-                "absPerf": stockReturnOneYear.round(2),
-                "relPerf": (stockReturnOneYear - indexReturnOneYear).round(2),
+                f"{period}": {
+                    "Return": stockReturnOneYear.round(2),
+                    "Return - SPY": (stockReturnOneYear - indexReturnOneYear).round(2),
+                }
             }
 
-            print(
-                f"{ticker} one year return vs SPY = {(stockReturnOneYear - indexReturnOneYear).round(2)}"
-            )
+            if (
+                f"{indexETF}" not in perf_rank
+                and f"{indexETF}" not in perf_rank[f"{period}"]
+            ):
+                perf_rank[f"{indexETF}"] = {
+                    f"{period}": {
+                        "Return": indexReturnOneYear.round(2),
+                        "Return - SPY": indexReturnOneYear.round(2),
+                    }
+                }
+
+            # {"NVDA": {"YTD": {"Return":22, "Return - SPY":18}, "oneYear": {"Return":22, "Return - SPY":18}},
+            #  "AAP": {"YTD": {"Return":22, "Return - SPY":18}, "oneYear": {"Return":22, "Return - SPY":18}}}
+
+            # print(
+            #     f"{ticker} one year return vs SPY = {(stockReturnOneYear - indexReturnOneYear).round(2)}"
+            # )
+
             # if (stockReturnOneYear - indexReturnOneYear) > 0:
             #     print(
             #         f"{ticker} one year return vs SPY = {(stockReturnOneYear - indexReturnOneYear).round(2)}"
@@ -357,11 +279,25 @@ for ticker in tickers:
     except Exception as e:
         print(f"{ticker} An error occurred: {e}")
 
-if f"{indexETF}" not in perf_rank:
-    perf_rank[f"{indexETF}"] = {"absPerf": indexReturnOneYear.round(2), "relPerf": 0}
+
+for ticker in tickers:
+    # print(f"{ticker}")
+    for period in return_periods:
+        if period == "YTD":
+            begDate = beg_period_date
+        elif period == "OneYear":
+            begDate = adjusted_one_year_ago
+        elif period == "TwoYears":
+            begDate = adjusted_two_year_ago
+
+        getPeriodReturn(begDate, endDate, period, ticker)
 
 sorted_perf_rank = dict(
-    sorted(perf_rank.items(), key=lambda x: x[1]["relPerf"], reverse=True)
+    sorted(perf_rank.items(), key=lambda x: x[1]["Return - SPY"], reverse=True)
 )
 
 perf_rank_df = pd.DataFrame.from_dict(sorted_perf_rank, orient="index")
+
+sharesPriceTrack = pd.concat(
+    [perf_rank_df["Return - SPY"], shares_reduction_slope], axis=1
+)
